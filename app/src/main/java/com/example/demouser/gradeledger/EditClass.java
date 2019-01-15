4ca8c357963044f625d109e00d087c45b194ce9e
Sara Ajmal Safi <safi22s@mtholyoke.edu>
2019-01-15 14:50:54 -0500

    Assignment - Third commit

--- app/src/main/java/com/example/demouser/gradeledger/EditClass.java
+++ app/src/main/java/com/example/demouser/gradeledger/EditClass.java
@@ -2,12 +2,20 @@ package com.example.demouser.gradeledger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.demouser.gradeledger.Model.Assignment;
 
 public class EditClass extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_class);
+
     }
+
+
 }
